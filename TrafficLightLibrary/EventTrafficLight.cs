d1cfaeaee78653a09b26ebc7fa5893e66fe2b297
Ivan <ivan.trashchenko@gmail.com>
2018-07-30 22:57:26 +0300

    Add events in Task 2

--- TrafficLightLibrary/EventTrafficLight.cs
+++ TrafficLightLibrary/EventTrafficLight.cs
@@ -9,6 +9,21 @@ namespace TrafficLightLibrary
     public class EventTrafficLight
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="TrafficLight"/> class with specific state.
+        /// Default - green.
+        /// </summary>
+        /// <param name="state">State of light of type <see cref="EventHandler"/>.</param>
+        public EventTrafficLight(EventHandler state = null)
+        {
+            if (state == null)
+            {
+                state = this.SwitchToGreen;
+            }
+
+            this.State += state;
+        }
+
+        /// <summary>
         /// State of light.
         /// </summary>
         public event EventHandler State = delegate { };
@@ -20,6 +35,58 @@ namespace TrafficLightLibrary
         {
             this.OnState(this, EventArgs.Empty);
         }
+
+        #region Switching methods
+
+        public void SwitchToGreen(object sender, EventArgs e)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.Write(i > 3 ? "Green light: {0}" : "Flashing green light: {0}", i);
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+            this.State = this.SwitchToYellow;
+        }
+
+        public void SwitchToRed(object sender, EventArgs e)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.Write("Red light: {0}", i);
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+            this.State = this.SwitchToRedAndYellow;
+        }
+
+        public void SwitchToRedAndYellow(object sender, EventArgs e)
+        {
+            for (int i = 3; i >= 0; i--)
+            {
+                Console.Write("Red and yellow light: {0}", i);
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+            this.State = this.SwitchToGreen;
+        }
+
+        public void SwitchToYellow(object sender, EventArgs e)
+        {
+            for (int i = 3; i >= 0; i--)
+            {
+                Console.Write("Yellow light: {0}", i);
+                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+            this.State = this.SwitchToRed;
+        }
+
+        #endregion    
 
         protected virtual void OnState(object sender, EventArgs e)
         {
